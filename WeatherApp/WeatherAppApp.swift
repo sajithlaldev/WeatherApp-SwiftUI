4b717b8a9764772dc43bc227f9925541c07ba8bc
sajithlaldev <sajithlal65@gmail.com>
2022-12-20 09:29:38 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by SajithlalDEV on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
